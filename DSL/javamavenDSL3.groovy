be2c184b1b09975d0b6c15f75417fae5484cd9dc
ctorresso <a210216376@unison.mx>
2022-04-19 17:14:18 -0400

    Push desde Terminal

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -20,7 +20,7 @@ job('Java Maven App DSL 3') {
           goals('test')
         }
         shell('''
-          echo "Entrega: @@@@@@@" 
+          echo "Entrega: Esta es una prueba desde Terminal" 
           java -jar "/var/jenkins_home/workspace/Java Maven App DSL 3/target/my-app-1.0-SNAPSHOT.jar"
         ''')  
     }
@@ -55,7 +55,7 @@ job('Job test Hola Mundo') {
     	}
 	steps {
 		shell('''
-			echo "TRAVEZZZZZ!!!!!"
+			echo "Prueba desde la terminal!!!!!"
 		''')
 	}
 }
